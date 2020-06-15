fecbfba92bde52da73c4f3a1b9a0f2b14a287763
francepants <franceline90@gmail.com>
2020-06-15 13:46:18 -0500

    initial commit

---
+++ lib/myCLIProject.rb
@@ -0,0 +1,6 @@
+require "myCLIProject/version"
+
+module MyCLIProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/myCLIProject/version.rb
@@ -0,0 +1,3 @@
+module MyCLIProject
+  VERSION = "0.1.0"
+end
